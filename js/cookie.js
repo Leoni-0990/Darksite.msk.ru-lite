8f6bc8e674323f83aec9edad08093c6b020c3651
Leoni-0990 <agressive@keemail.me>
2024-12-26 18:24:08 +0300

    Leoni-0990
    v0.1.2
    
    Добавлены Cookie
    Изменен JS
    Удалены ненужные строки кода + используется Jqery

---
+++ js/cookie.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".warning");
+if (document.cookie.indexOf("warning") != -1) {
+  container.classList.remove("warning--active");
+}
+
+if (document.querySelector(".warning")) {
+  const apply = container.querySelector(".warning__apply");
+  apply.addEventListener("click", () => {
+    container.classList.remove("warning--active");
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,34 +1,44 @@
-function present() {
-  const headName = document.getElementById("headName");
-  const heightPage = window.innerHeight;
-  console.log(heightPage);
-  const head = document.getElementById("head");
-  headAuteHeight = getComputedStyle(head).height;
-  console.log(headAuteHeight);
-  head.style.height = heightPage + "px";
 
-  function headAni1() {
-    head.style.height = headAuteHeight;
+
+
+
+document.cookie = "user=true;path=/"; // обновляем только куки с именем 'user'
+
+var results = document.cookie.match(/user=(.+?)(;|$)/);
+var user_cookie = results[1]; // user
+console.log(user_cookie);
+
+if (user_cookie == 'true'){
+  window.onload = function present() {
+    const headName = document.getElementById("headName");
+    const heightPage = window.innerHeight;
+    const head = document.getElementById("head");
+    headAuteHeight = getComputedStyle(head).height;
+    head.style.height = heightPage + "px";
+  
+    function headAni1() {
+      head.style.height = headAuteHeight;
+    }
+    function headAni2() {
+      headName.style.fontSize = "2em";
+    }
+    setTimeout(headAni2, 1200);
+    setTimeout(headAni1, 1700);  
   }
-  function headAni2() {
-    headName.style.fontSize = "2em";
-  }
-  setTimeout(headAni2, 1200);
-  setTimeout(headAni1, 1700);
+} else {
+  console.log('Ошибка JS!')
 }
-window.onload = present;
+
 function copyText() {
+  let flash_box = document.getElementById("flash_box");
   let speed_copy = "2000";
   navigator.clipboard.writeText("leonid15000@vk.com");
-  let flash_box = document.getElementById("flash_box");
-  const hph = "flash_box " + speed_copy + " ms";
   flash_box.style.animation = "flash_box " + speed_copy + "ms";
-  console.log(hph);
+
   function flash_box_none() {
     flash_box.style.animation = "none";
   }
   setTimeout(flash_box_none, speed_copy);
 }
-function mail_flash(){
-  
-}
+function mail_flash() {}
+
